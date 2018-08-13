180644f0dfd40375b0ed5a18bb453c2cea345ccc
myasinK <yasinul@gmail.com>
2018-08-13 15:11:27 -0700

    Finish drill 5

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace BooleanLogicIntro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is your age?");
+            int age = Convert.ToInt16( Console.ReadLine() );
+            Console.WriteLine("Have you ever had a DUI? (true/false)");
+            bool dui = Convert.ToBoolean(Console.ReadLine());
+            Console.WriteLine("How many speeding tickets do you have?");
+            int tickets = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("Qualified?");
+            Console.WriteLine( (age > 15) && ( dui == false ) && ( tickets <= 3 ) );
+            Console.ReadLine();
+        }
+    }
+}
